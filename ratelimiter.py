cc55a22fe30b5e520261e1057247d531a261b316
Ingmar Sell <ingmarsell1@gmail.com>
2019-06-20 09:24:52 -0400

    Implement ratelimiter

--- ratelimiter.py
+++ ratelimiter.py
@@ -1,1 +1,15 @@
- 
+previousTime = {}
+
+def rateLimit(name, delay):
+    currentTime = int(round(time.time() * 1000))
+    #Name called for the first time
+    if name not in previousTime:
+        previousTime.update( {name : currentTime} )
+        return False
+    #Name has called been called too fast
+    elif currentTime - previousTime[name] < delay:
+        return True
+    #Name is allowed
+    else:
+        previousTime[name] = currentTime
+        return False
